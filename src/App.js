880ff113a90cde67ca57a4e68d5f08ca0a4a619c
SabahNur86 <sabahnurdagtekin@gmail.com>
2022-01-19 21:30:59 +0300

    privateagile1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/HomePage";
+import Footer from "./components/common/Footer";
+import Topbar from "./components/common/Topbar";
+import CustomRoutes from "./router/CustomRoutes";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Topbar />
+        <CustomRoutes />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Topbar from "../components/common/Topbar";
+
 import Banner from "../components/home/banner/Banner";
 import BannerContact from "../components/home/banner/BannerContact";
 import Counters from "../components/home/counter/Counters";
 import Features from "../components/home/Features";
-import HomeAbout from "../components/home/homeAbout/HomeAbout";
+import HomeAbout from "../components/home/HomeAbout";
 
 const HomePage = () => {
   return (
     <>
-      <Topbar />
       <Banner />
       <BannerContact />
       <Features />
